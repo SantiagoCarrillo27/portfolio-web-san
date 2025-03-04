be1ca0556089a18ddbb8136152cbf9161010ba33
Santiago Carrillo <juniorcarrilo14@gmail.com>
2025-03-04 18:09:14 +0100

    Update components and configuration

---
+++ src/util/ProjectsMain.ts
@@ -0,0 +1,62 @@
+
+// SE IMPORTAN LOS ICONOS DE LAS TECNOLOGIAS
+import {TAGS} from "@util/TagsIcons";
+
+
+
+export const PROJECTS = [
+    {
+      title: "BeautyBook",
+      description:
+        "App web para el agendamiento de citas. Autenticación y confirmación via Email, gestión de servicios, roles de cliente y admin, manejo de horarios y más.  ",
+      link: "https://google.com",
+      image: "/projects/image.webp",
+      tags: [TAGS.PHP, TAGS.SASS, TAGS.JAVASCRIPT,TAGS.MYSQL],
+    },
+    {
+      title: "GifsApp",
+      description:
+        "Plataforma didáctica para la búsqueda de GIFS divertidos y modernos, con filtrado integrado, manejo de sesión, guardado de búsquedas anteriores y más.",
+      link: "https://santiagocarrillo27.github.io/gifsApp/",
+      github:"https://github.com/SantiagoCarrillo27/gifsApp",
+      image: "/projects/appGifs.webp",
+      tags: [TAGS.ANGULAR, TAGS.BOOTSTRAP, TAGS.TYPESCRIPT],
+    },
+  ];
+
+
+  export const OTHER_PROJECTS = [
+    {
+      title: "CountryApp",
+      description:
+        "Poder ver la cantidad de población de un país y sus capitales, además de sus banderas",
+      link: "https://santiagocarrillo27.github.io/gifsApp/",
+      github:"https://github.com/SantiagoCarrillo27/gifsApp",
+      tags: [TAGS.ANGULAR, TAGS.BOOTSTRAP, TAGS.TYPESCRIPT],
+    },
+    {
+        title: "CountryApp",
+        description:
+          "Plataforma didáctica para la búsqueda de GIFS divertidos y modernos, con filtrado integrado, manejo de sesión, guardado de búsquedas anteriores y más.",
+        link: "https://santiagocarrillo27.github.io/gifsApp/",
+        github:"https://github.com/SantiagoCarrillo27/gifsApp",
+        tags: [TAGS.ANGULAR, TAGS.BOOTSTRAP, TAGS.TYPESCRIPT],
+      },
+      {
+        title: "CountryApp",
+        description:
+          "Plataforma didáctica para la búsqueda de GIFS divertidos y modernos, con filtrado integrado, manejo de sesión, guardado de búsquedas anteriores y más.",
+        link: "https://santiagocarrillo27.github.io/gifsApp/",
+        github:"https://github.com/SantiagoCarrillo27/gifsApp",
+        tags: [TAGS.ANGULAR, TAGS.BOOTSTRAP, TAGS.TYPESCRIPT],
+      },
+      {
+        title: "CountryApp",
+        description:
+          "Plataforma didáctica para la búsqueda de GIFS divertidos y modernos, con filtrado integrado, manejo de sesión, guardado de búsquedas anteriores y más.",
+        link: "https://santiagocarrillo27.github.io/gifsApp/",
+        github:"https://github.com/SantiagoCarrillo27/gifsApp",
+        tags: [TAGS.ANGULAR, TAGS.BOOTSTRAP, TAGS.TYPESCRIPT],
+      }
+
+  ]
---
+++ src/util/TagsIcons.ts
@@ -0,0 +1,48 @@
+
+import PHP from "@icons/PHPIcon.astro";
+import SASS from "@icons/SassIcon.astro";
+import JAVASCRIPT from "@icons/JavaScriptIcon.astro";
+import ANGULAR from "@icons/AngularIcon.astro";
+import BOOTSTRAP from "@icons/BootstrapIcon.astro";
+import MYSQL from "@icons/MysqlIcon.astro";
+import TYPESCRIPT from "@icons/TypeScript.astro";
+
+ export const TAGS = {
+  PHP: {
+    name: "PHP",
+    class: "text-black dark:text-white",
+    icon: PHP,
+  },
+  ANGULAR: {
+    name: "Angular",
+    class: "text-white",
+    icon: ANGULAR,
+  },
+  SASS: {
+    name: "Sass CSS",
+    class: "",
+    icon: SASS,
+  },
+  JAVASCRIPT: {
+    name: "JavaScript",
+    class: "",
+    icon: JAVASCRIPT,
+  },
+  BOOTSTRAP: {
+    name: "Bootstrap",
+    class: "",
+    icon: BOOTSTRAP,
+  },
+  MYSQL: {
+    name: "MySql",
+    class: "",
+    icon: MYSQL,
+  },
+  TYPESCRIPT: {
+    name: "TypeScript",
+    class: "",
+    icon: TYPESCRIPT,
+  },
+};
+
+
